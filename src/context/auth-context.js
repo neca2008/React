2171af0c7b3a9aace8886d906eac6808ed363d04
Ines Gomes Ramos <inramos@deloitte.pt>
2019-10-14 11:19:22 +0100

    start projetc

--- src/context/auth-context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const authContext = React.createContext({
-    //default values onl apply when no ther values are set
-    autheticated: false,
-    login: () => { }
-});
-
-export default authContext;
--- src/hoc/Auxiliary.js
+++
@@ -1,7 +0,0 @@
-
-
-//children will refer t the content between the opening and closin tag of its content
-//same as React Fragment!!
-const aux = props => props.children;
-
-export default aux;
