90a5b3caeef666e8d8b8cb351f5ad84fcd177774
Ines Gomes Ramos <inramos@deloitte.pt>
2019-10-14 16:59:28 +0100

    adding and removing with price

--- myburger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ myburger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 const INGREDIENT_PRICES = {
     salad:0.5,
-    chees:0.4,
+    cheese:0.4,
     bacon:1,
     meat:2.2
 }
@@ -38,15 +38,37 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) =>{
-        
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <=0 ){
+            return;
+        }
+        const updatedCount = oldCount -1;
+        const updatedIngrediets ={
+            ...this.state.ingredients
+        };
+        updatedIngrediets[type]=updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({totalPrice:newPrice,ingredients:updatedIngrediets})
     }
     
     render() {
+        const disabledInfo ={
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo){
+            disabledInfo[key]= disabledInfo[key] <=0
+        }
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
-                ingredientAdded={this.addIngredientHandler}/>
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved ={this.removeIngredientHandler}
+                disabled={disabledInfo}
+                price={this.state.totalPrice}/>
             </Auxiliary>
         );
     }
